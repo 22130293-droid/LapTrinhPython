dc4a7a6a01ce611ae3342fd1680d15aafc826529
Trần Văn Đồng <146536282+tran-van-dong@users.noreply.github.com>
2025-12-17 16:34:10 +0700

    Add files via upload

---
+++ booking_and_voice_search/voice_controller.py
@@ -0,0 +1,25 @@
+# movie_recommender_ai_module/voice_search/voice_controller.py
+
+from .speech_to_text import SpeechToText
+
+
+class VoiceSearchController:
+    def __init__(self):
+        self.stt = SpeechToText(language="vi-VN")
+
+    def get_voice_query(self):
+        """
+        Kích hoạt micro và lấy nội dung tìm kiếm
+        """
+        result = self.stt.listen()
+
+        if result == "TIMEOUT":
+            return None, "⏱️ Không nghe thấy giọng nói"
+
+        if result == "UNKNOWN":
+            return None, " Không nhận diện được giọng nói"
+
+        if result.startswith("ERROR"):
+            return None, " Lỗi micro"
+
+        return result, None
